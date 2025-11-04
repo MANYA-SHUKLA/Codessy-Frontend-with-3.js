b1e2e8cd3b7989e76d7afb513fd560a328c6a2be
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2025-11-04 17:49:53 +0530

    Remove comments from ParallaxBackground component

--- src/app/components/UI/ParallaxBackground.tsx
+++ src/app/components/UI/ParallaxBackground.tsx
@@ -15,13 +15,13 @@ export default function ParallaxBackground() {
 
   return (
     <div ref={ref} className="fixed inset-0 -z-20 pointer-events-none">
-      {/* Background Layer - Slowest */}
+ 
       <motion.div 
         style={{ y: backgroundY }}
         className="absolute inset-0 bg-gradient-to-br from-slate-900 via-purple-900/20 to-blue-900/30"
       />
       
-      {/* Midground Layer - Medium */}
+
       <motion.div 
         style={{ y: midgroundY }}
         className="absolute inset-0"
@@ -30,7 +30,7 @@ export default function ParallaxBackground() {
         <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl" />
       </motion.div>
       
-      {/* Foreground Layer - Fastest */}
+
       <motion.div 
         style={{ y: foregroundY }}
         className="absolute inset-0"
